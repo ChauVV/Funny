4908649bae14bcbfbbd374dca1db4b94e2cf7dcd
Jeremy <nhokdat0803@gmail.com>
2019-11-23 15:24:23 +0700

    commit

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,10 +1,14 @@
 const Images = {
-  homeBg: require('./home/home_bg.png'),
   bgLoginTablet: require('./login/bgLoginTablet.jpg'),
   icnAgent: require('./login/icnAgent.png'),
   icnPassword: require('./login/icnPassword.png'),
   icnHidePass: require('./login/icnHidePass.png'),
   icn_viewPass: require('./login/icn_viewPass.png'),
-  genCareerLogo: require('./display/GenCareer_Logo.png')
+  genCareerLogo: require('./display/GenCareer_Logo.png'),
+  imgTemp: require('./home/imgTemp2.png'),
+  icLoveOn: require('./home/ic_love_on.png'),
+  icComment: require('./home/ic_comment.png'),
+  icMore: require('./home/ic_more.png'),
+  icNext: require('./home/ic_next.png')
 }
 export default Images
